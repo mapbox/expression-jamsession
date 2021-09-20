5ac75bc10b397f1e06cd49c23cd7aba6c33dba88
Derek Lieu <derek.lieu@mapbox.com>
2021-09-20 15:16:28 -0700

    Resolve external deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
-import babel from 'rollup-plugin-babel';
+import { babel } from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
@@ -10,6 +12,8 @@ export default {
   plugins: [
     babel({
       exclude: 'node_modules/**'
-    })
+    }),
+    commonjs(),
+    resolve()
   ]
 };
